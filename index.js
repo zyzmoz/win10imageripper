340256a62b01041c25f77b1996ddf0eea0376475
Daniel Cunha <danielbrandaodacunha@gmail.com>
2019-09-10 13:34:23 -0300

    Loop fixes

--- index.js
+++ index.js
@@ -7,47 +7,46 @@ const directoryPath = path.join('/Users', `${userName}/AppData/Local/Packages/Mi
 
 
 const fetchImages = (output = __dirname) => {
-  const targetPath = path.join(output, 'temp');
-  fs.readdirSync(directoryPath, (err, files) => {
-    if (err)
-      return console.log(err);
+  return new Promise((resolve) => {
+    const targetPath = path.join(output, 'temp');
+    fs.readdir(directoryPath, (err, files) => {
+      if (err)
+        return console.log(err);
 
-    files.forEach(file => {
-      if (!fs.existsSync('temp'))
-        fs.mkdirSync('temp');
-      
-      const targetFile = `${targetPath}/${file}.temp.jpg`
-      fs.copyFile(`${directoryPath}/${file}`, targetFile, (err, file) => {
-        if (err)
-          return console.log(err);
-        
-        sizeOf(targetFile, (err, size) => {
-          if (err) {
-            fs.unlink(targetFile, (err) => {
-              if (err)
-                return console.log(err)
-            });
+      files.forEach(file => {
+        if (!fs.existsSync('temp'))
+          fs.mkdirSync('temp');
+
+        const targetFile = `${targetPath}/${file}.temp.jpg`
+        fs.copyFile(`${directoryPath}/${file}`, targetFile, (err, file) => {
+          if (err)
             return console.log(err);
-          }
-          if ((size.width <= 500) || err)
-            fs.unlink(targetFile, (err) => {
-              if (err)
-                return console.log(err)
-            });
+
+          sizeOf(targetFile, (err, size) => {
+            if (err) {
+              fs.unlink(targetFile, (err) => {
+                if (err)
+                  return console.log(err)
+              });
+              return console.log(err);
+            }
+            if ((size.width <= 500) || err)
+              fs.unlink(targetFile, (err) => {
+                if (err)
+                  return console.log(err)
+              });
+          });
         });
+      });
+      const outputPath = path.join(output, 'temp');
+      fs.readdir(outputPath, (err, files) => {
+        let res = []
+        files.forEach(file => res.push(`${targetPath}/${file}`));
+        resolve(res);
       });
     });
   });
-  const outputPath = path.join(output, 'temp');
-  console.log(outputPath);
-  return new Promise((resolve) => fs.readdir(outputPath, (err, files) => {
-    let res = [] 
-    console.log('files', files);
-    files.forEach(file => res.push(`${targetPath}/${file}`));
-    resolve(res);
-  }));
-  
-  
+
 }
 
 module.exports = {
